cf98c0b8549ece61ae24f950859c0832368fa250
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2023-06-20 16:41:09 -0300

    Update UpdateServicesAWS_6-2023.sh

--- UpdateServicesAWS_6-2023.sh
+++ UpdateServicesAWS_6-2023.sh
@@ -53,6 +53,17 @@ log "Removendo arquivos temporários...."
 # Remove packages
 rm *.deb
 
+log "Instalando Intel Graphics"
+#Install Intel Graphics
+sudo su
+mkdir -p /etc/X11/xorg.conf.d
+touch 20-intel.conf
+echo 'Section "Device"
+  Identifier "Intel Graphics"
+  Driver "intel"
+  Option "TearFree" "true"
+EndSection' >>/etc/X11/xorg.conf.d/20-intel.conf
+
 log "Reiniciando...."
 echo "*****************Instalação Concluida*************************"
 reboot
