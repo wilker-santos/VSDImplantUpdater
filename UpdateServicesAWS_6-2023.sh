09ee263be3f9f5b7010e5c3cd0e54e58c791fdb3
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2023-10-11 15:55:31 -0300

    Update UpdateServicesAWS_6-2023.sh

--- UpdateServicesAWS_6-2023.sh
+++ UpdateServicesAWS_6-2023.sh
@@ -68,26 +68,7 @@ rm vs-food.sh
 
 log "Restaurando Backups...."
 # Restaurar Backups
-sudo mv /opt/videosoft_bkp_log/vs-autopag-se/log/*2023-07* /opt/videosoft/vs-autopag-se/log/
-sudo mv /opt/videosoft_bkp_log/vs-autopag-se/log/*2023-06* /opt/videosoft/vs-autopag-se/log/
-sudo mv /opt/videosoft_bkp_log/vs-autopag-se/log/*2023-05* /opt/videosoft/vs-autopag-se/log/
-sudo mv /opt/videosoft_bkp_log/vs-autopag-se/log/*2023-04* /opt/videosoft/vs-autopag-se/log/
-
-sudo mv /opt/videosoft_bkp_log/vs-autopag-se/log/dmp/*202307* /opt/videosoft/vs-autopag-se/log/dmp/
-sudo mv /opt/videosoft_bkp_log/vs-autopag-se/log/dmp/*202306* /opt/videosoft/vs-autopag-se/log/dmp/
-sudo mv /opt/videosoft_bkp_log/vs-autopag-se/log/dmp/*202305* /opt/videosoft/vs-autopag-se/log/dmp/
-sudo mv /opt/videosoft_bkp_log/vs-autopag-se/log/dmp/*202304* /opt/videosoft/vs-autopag-se/log/dmp/
-
-sudo mv /opt/videosoft_bkp_log/vs-os-interface/log/*2023-07* /opt/videosoft/vs-os-interface/log/
-sudo mv /opt/videosoft_bkp_log/vs-os-interface/log/*2023-06* /opt/videosoft/vs-os-interface/log/
-sudo mv /opt/videosoft_bkp_log/vs-os-interface/log/*2023-05* /opt/videosoft/vs-os-interface/log/
-sudo mv /opt/videosoft_bkp_log/vs-os-interface/log/*2023-04* /opt/videosoft/vs-os-interface/log/
-sudo mv /opt/videosoft_bkp_log/vs-os-interface/log/_data* /opt/videosoft/vs-os-interface/log/
-
-sudo mv /opt/videosoft_bkp_log/vs-print/log/*2023-07* /opt/videosoft/vs-print/log/
-sudo mv /opt/videosoft_bkp_log/vs-print/log/*2023-06* /opt/videosoft/vs-print/log/
-sudo mv /opt/videosoft_bkp_log/vs-print/log/*2023-05* /opt/videosoft/vs-print/log/
-sudo mv /opt/videosoft_bkp_log/vs-print/log/*2023-04* /opt/videosoft/vs-print/log/
+# Em Construção
 
 log "Instalação Concluida"
 echo "*****************Instalação Concluida*************************"
