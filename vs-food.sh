9af74aba08295d78d21ba5efc9a21238cde4d03f
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2023-10-11 15:35:04 -0300

    Add files via upload

---
+++ vs-food.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+VS_URL_APP="https://food2.vsd.app"
+
+# Parâmetros a ser passado ao Navegador
+PARAMS="--password-store=basic --kiosk --disable-pinch --disable-component-update"
+
+# Abre navegador avisando que vai atualizar o navegador
+# echo "Aguarde, atualizando o navegador..."
+# google-chrome /opt/videosoft/vs-food-launcher/app/updating-browser.html $PARAMS &>/dev/null &
+# echo "Abriu o navegador, aguarde..."
+
+# Atualiza navegador
+# echo "Chamando Os Interface para atualizar o navegador..."
+# curl http://localhost:3300/update/chrome
+# echo "Passou pelo update do chrome"
+# killall chrome
+# echo "Fechou chrome com mensagem de atualização"
+# sleep 3
+
+# Abre a aplicação
+echo "Abrindo a aplicação..."
+google-chrome "$VS_URL_APP" $PARAMS
+
