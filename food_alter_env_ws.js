52bcb21750c89571d134b60b37de39c6454549c6
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2023-11-09 08:56:21 -0300

    Update food_alter_env_ws.js

--- food_alter_env_ws.js
+++ food_alter_env_ws.js
@@ -5,8 +5,8 @@ let env_ws = document.getElementById('env_ws');
 // Converte em JSON a Configuração de Terminais
 let env_ws_json = JSON.parse(env_ws.textContent);
 // Altera o Atributo do Objeto [Aqui serão incluidos os parametros a serem alterados pelo Script]
-env_ws_json.DOCK.APPS[0].url = 'https://food2.vsd.app?checkModules=false';
-env_ws_json.DOCK.APPS[1].url = 'https://selfcheckout.vsd.app?checkModules=false';
+env_ws_json.DOCK.APPS[0].url = 'https://food2.vsd.app/';
+// env_ws_json.DOCK.APPS[1].url = 'https://selfcheckout.vsd.app?checkModules=false';
 // Converte o JSON alterado em String e atribui ao Textarea indentado
 env_ws.textContent = JSON.stringify(env_ws_json, null, 4);
 // Salva as Alterações
