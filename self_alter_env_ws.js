bdf0f4004cc6baa95bb3e6405b48165f055e594f
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2023-11-22 12:19:27 -0300

    Update self_alter_env_ws.js

--- self_alter_env_ws.js
+++ self_alter_env_ws.js
@@ -1,6 +1,7 @@
 let save = document.getElementsByClassName('btn btn-primary btn-sm');
 let env_ws = document.getElementById('env_ws');
 let env_ws_json = JSON.parse(env_ws.textContent);
-env_ws_json.DOCK.BACK_TO_URL = 'https://food2.vsd.app?checkModules=false';
+// env_ws_json.DOCK.BACK_TO_URL = 'https://food2.vsd.app?checkModules=false';
+env_ws_json.PAYMENT.MULTIPLE_CARDS.ENABLED = 'false'
 env_ws.textContent = JSON.stringify(env_ws_json, null, 4);
 save[0].click();
