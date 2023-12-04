a25f2b6217a2fe77c80903e3fd687f722e02e642
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2023-12-04 08:29:56 -0300

    Update food_alter_env_ws.js

--- food_alter_env_ws.js
+++ food_alter_env_ws.js
@@ -6,7 +6,7 @@ let env_ws = document.getElementById('env_ws');
 let env_ws_json = JSON.parse(env_ws.textContent);
 // Altera o Atributo do Objeto [Aqui serão incluidos os parametros a serem alterados pelo Script]
 // env_ws_json.PAYMENT.MULTIPLE_CARDS_ACCEPTED_PAYING_METHODS = 'credito, debito';
-env_ws_json["COLLECT_NAME_AMOUNT_OF_CHARACTERS"] = "11";
+env_ws_json["COLLECT_NAME_AMOUNT_OF_CHARACTERS"] = "40";
 // Converte o JSON alterado em String e atribui ao Textarea indentado
 env_ws.textContent = JSON.stringify(env_ws_json, null, 4);
 // Salva as Alterações
