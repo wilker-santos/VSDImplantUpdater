4580939d4cdf5a93b209089aee06352bc3199450
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2023-12-06 12:20:20 -0300

    Update Install_Chromium.sh

--- Install_Chromium.sh
+++ Install_Chromium.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# sudo wget -O- https://raw.githubusercontent.com/wilker-santos/VSDImplantUpdater/main/Install_Chromium.sh | bash
 sudo apt install chromium-browser -y
 
 wget -c "https://raw.githubusercontent.com/wilker-santos/VSDImplantUpdater/main/vs-food.sh"
