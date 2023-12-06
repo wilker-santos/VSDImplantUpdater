681fb9fc1bfb54a54e84c023e6951b8d5e078d4f
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2023-12-06 13:47:13 -0300

    Update Install_Chromium.sh

--- Install_Chromium.sh
+++ Install_Chromium.sh
@@ -4,4 +4,3 @@ sudo apt install chromium-browser -y
 
 wget -c "https://raw.githubusercontent.com/wilker-santos/VSDImplantUpdater/main/vs-food.sh"
 sudo mv vs-food.sh /opt/videosoft/vs-food-launcher/app/vs-food.sh
-rm *sh
