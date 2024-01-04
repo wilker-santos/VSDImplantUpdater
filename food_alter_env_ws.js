14049d6d213dcef1594ab6099477f6d0ed1141c2
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2024-01-04 17:03:24 -0300

    Update food_alter_env_ws.js

--- food_alter_env_ws.js
+++ food_alter_env_ws.js
@@ -5,8 +5,11 @@ let env_ws = document.getElementById('env_ws');
 // Converte em JSON a Configuração de Terminais
 let env_ws_json = JSON.parse(env_ws.textContent);
 // Altera o Atributo do Objeto [Aqui serão incluidos os parametros a serem alterados pelo Script]
+env_ws_json["USE_COUPON"] = "true";
+env_ws_json["USE_COUPON_MESSAGE_INSTRUCTION"] = "Olá! Você tem um cupom de desconto?";
+env_ws_json["USE_COUPON_MESSAGE_INSERT"] = "Insira aqui o seu cupom de desconto KFC.";
 // env_ws_json.PAYMENT.MULTIPLE_CARDS_ACCEPTED_PAYING_METHODS = 'credito, debito';
-env_ws_json["COLLECT_NAME_AMOUNT_OF_CHARACTERS"] = "40";
+// env_ws_json["COLLECT_NAME_AMOUNT_OF_CHARACTERS"] = "40";
 // Converte o JSON alterado em String e atribui ao Textarea indentado
 env_ws.textContent = JSON.stringify(env_ws_json, null, 4);
 // Salva as Alterações
