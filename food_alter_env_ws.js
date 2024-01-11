3f88c224d37c3a3800cf6dc3308e59f4cd5b3ac6
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2024-01-11 11:03:50 -0300

    Update food_alter_env_ws.js

--- food_alter_env_ws.js
+++ food_alter_env_ws.js
@@ -12,9 +12,9 @@ let sel_lay_imp = document.getElementById('entity_print_layout_id_1');
 // env_ws_json["USE_COUPON_MESSAGE_INSERT"] = "Insira aqui o seu cupom de desconto KFC.";
 // env_ws_json.PAYMENT.MULTIPLE_CARDS_ACCEPTED_PAYING_METHODS = 'credito, debito';
 // env_ws_json["COLLECT_NAME_AMOUNT_OF_CHARACTERS"] = "40";
-env_ws_json["CUSTOM_STEP_INTEGRATING"]["USES_CUSTOM_ICON"] = "false";
-env_ws_json["CUSTOM_STEP_INTEGRATING"]["TITLE"] = "Enviando o seu pedido para nossa cozinha!";
-env_ws_json["CUSTOM_STEP_INTEGRATING"]["SUBTITLE"] = "Pedido realizado!\nAguarde que estamos imprimindo sua comanda, e caso deseje, retire seu cupom fiscal no balcão";
+env_ws_json["CUSTOM_STEP_INTEGRATING"].USES_CUSTOM_ICON = "false";
+env_ws_json["CUSTOM_STEP_INTEGRATING"].TITLE = "Enviando o seu pedido para nossa cozinha!";
+env_ws_json["CUSTOM_STEP_INTEGRATING"].SUBTITLE = "Pedido realizado!\nAguarde que estamos imprimindo sua comanda, e caso deseje, retire seu cupom fiscal no balcão";
 sel_lay_imp.value = 20;
 // Converte o JSON alterado em String e atribui ao Textarea indentado
 env_ws.textContent = JSON.stringify(env_ws_json, null, 4);
