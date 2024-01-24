49a58944fa0ad59d820b5ccfba57ad198e5dc635
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2024-01-24 16:25:38 -0300

    Update UpdateServicesAlfa.sh

--- UpdateServicesAlfa.sh
+++ UpdateServicesAlfa.sh
@@ -67,11 +67,11 @@ if [[ $cpu_model_lower == *i5* ]]; then
     wget --inet4-only -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     log "Instalando Google Chrome...."
     sudo dpkg -i google-chrome-stable_current_amd64.deb
-    google-chrome
+    google-chrome &>/dev/null &
     sleep 5
     killall chrome
+    sleep 3
     # Nome do arquivo de configuração
-    #/snap/chromium/common/chromium/Default
     arquivo_configuracao="/home/videosoft/.config/google-chrome/Default/Preferences"
     # Adiciona o trecho no arquivo
     sed -i -E 's/"autofill":\s*\{[^\}]*"last_version_deduped":\s*120\}/"autofill":\{"last_version_deduped":120,"profile_enabled":false\}/' "$arquivo_configuracao"
@@ -89,9 +89,10 @@ elif [[ $cpu_model_lower == *i3* ]]; then
     wget -c "https://raw.githubusercontent.com/wilker-santos/VSDImplantUpdater/main/vs-food.sh"
     log "Movendo Script VS Food For Chromium...."
     sudo mv vs-food.sh /opt/videosoft/vs-food-launcher/app/vs-food.sh
-    chromium
+    chromium &>/dev/null &
     sleep 5
     killall chrome
+    sleep 3
     # Nome do arquivo de configuração
     arquivo_configuracao="/home/videosoft/snap/chromium/common/chromium/Default/Preferences"
     # Adiciona o trecho no arquivo
