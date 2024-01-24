990bdec8cc81cff68046b2666eb4378ac87c8b41
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2024-01-24 16:13:09 -0300

    Update Chromium.sh

--- Chromium.sh
+++ Chromium.sh
@@ -21,3 +21,30 @@ if [[ $cpu_model_lower == *i5* ]]; then
     # Adiciona o trecho no arquivo
     sed -i -E 's/"autofill":\s*\{[^\}]*"last_version_deduped":\s*120\}/"autofill":\{"last_version_deduped":120,"profile_enabled":false\}/' "$arquivo_configuracao"
     log "Desabilitado Preenchimento Automatico...."
+elif [[ $cpu_model_lower == *i3* ]]; then
+   # log "Download Chromium...."
+   # wget --inet4-only -c http://packages.linuxmint.com/pool/upstream/c/chromium/chromium_120.0.6099.216%7elinuxmint1%2buna_amd64.deb
+    sudo apt update
+    log "Instalando Chromium...."
+    echo "Instalando Chromium...."
+    sudo snap install chromium
+    echo "Chromium Instalado...."
+   # sudo dpkg -i chromium_120.0.6099.216~linuxmint1+una_amd64.deb
+    log "Download Script VS Food For Chromium...."
+    wget -c "https://raw.githubusercontent.com/wilker-santos/VSDImplantUpdater/main/vs-food.sh"
+    log "Movendo Script VS Food For Chromium...."
+    sudo mv vs-food.sh /opt/videosoft/vs-food-launcher/app/vs-food.sh
+    chromium
+    sleep 5
+    killall chrome
+    # Nome do arquivo de configuração
+    arquivo_configuracao="/home/videosoft/snap/chromium/common/chromium/Default/Preferences"
+    # Adiciona o trecho no arquivo
+    sed -i -E 's/"autofill":\s*\{[^\}]*"last_version_deduped":\s*120\}/"autofill":\{"last_version_deduped":120,"profile_enabled":false\}/' "$arquivo_configuracao"
+    log "Desabilitado Preenchimento Automatico...."
+else
+    log "Download Google Chrome...."
+    wget --inet4-only -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    log "Instalando Google Chrome...."
+    sudo dpkg -i google-chrome-stable_current_amd64.deb
+fi
