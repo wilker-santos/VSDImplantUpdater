c8f98c693395f30dd583bd4291cc2ffc62f678f0
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2024-01-25 13:15:46 -0300

    Update UpdateServicesAlfa.sh

--- UpdateServicesAlfa.sh
+++ UpdateServicesAlfa.sh
@@ -63,20 +63,7 @@ cpu_model=$(lscpu | grep "Nome do modelo" | cut -d ':' -f 2 | sed 's/^ *//')
 cpu_model_lower=$(echo "$cpu_model" | tr '[:upper:]' '[:lower:]')
 
 # Verifica o modelo da CPU e instala o navegador correspondente
-if [[ $cpu_model_lower == *i5* ]]; then
-    log "Download Google Chrome...."
-    wget --inet4-only -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    log "Instalando Google Chrome...."
-    sudo dpkg -i google-chrome-stable_current_amd64.deb
-    google-chrome &>/dev/null &
-    sleep 5
-    killall chrome
-    # Nome do arquivo de configuração
-    arquivo_configuracao="/home/videosoft/.config/google-chrome/Default/Preferences"
-    # Adiciona o trecho no arquivo
-    sed -i -E 's/"autofill":\s*\{[^\}]*"last_version_deduped":\s*120\}/"autofill":\{"last_version_deduped":120,"profile_enabled":false\}/' "$arquivo_configuracao"
-    log "Desabilitado Preenchimento Automatico...."
-elif [[ $cpu_model_lower == *i3* ]]; then
+if [[ $cpu_model_lower == *i3* ]]; then
    # log "Download Chromium...."
    # wget --inet4-only -c http://packages.linuxmint.com/pool/upstream/c/chromium/chromium_120.0.6099.216%7elinuxmint1%2buna_amd64.deb
     log "Instalando Chromium...."
@@ -97,11 +84,20 @@ elif [[ $cpu_model_lower == *i3* ]]; then
     # Adiciona o trecho no arquivo
     sed -i -E 's/"autofill":\s*\{[^\}]*"last_version_deduped":\s*120\}/"autofill":\{"last_version_deduped":120,"profile_enabled":false\}/' "$arquivo_configuracao"
     log "Desabilitado Preenchimento Automatico...."
+#if [[ $cpu_model_lower == *i5* ]]; then
 else
     log "Download Google Chrome...."
     wget --inet4-only -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     log "Instalando Google Chrome...."
     sudo dpkg -i google-chrome-stable_current_amd64.deb
+    google-chrome &>/dev/null &
+    sleep 5
+    killall chrome
+    # Nome do arquivo de configuração
+    arquivo_configuracao="/home/videosoft/.config/google-chrome/Default/Preferences"
+    # Adiciona o trecho no arquivo
+    sed -i -E 's/"autofill":\s*\{[^\}]*"last_version_deduped":\s*120\}/"autofill":\{"last_version_deduped":120,"profile_enabled":false\}/' "$arquivo_configuracao"
+    log "Desabilitado Preenchimento Automatico...."
 fi
 
 # IF Chrome Beta
