4c2f1c0bfec91cc68663210607dba81bf0bb0e3a
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2024-01-30 14:17:03 -0300

    Update food_alter_env_ws.js

--- food_alter_env_ws.js
+++ food_alter_env_ws.js
@@ -4,23 +4,9 @@ let save = document.getElementsByClassName('btn btn-primary btn-sm');
 let env_ws = document.getElementById('env_ws');
 // Converte em JSON a Configuração de Terminais
 let env_ws_json = JSON.parse(env_ws.textContent);
-// Obtem Seletor Layouts de Impressão Comanda
-// let sel_lay_imp = document.getElementById('entity_print_layout_id_1');
-// Altera o Atributo do Objeto [Aqui serão incluidos os parametros a serem alterados pelo Script]
-// env_ws_json["USE_COUPON"] = "true";
-// env_ws_json["USE_COUPON_MESSAGE_INSTRUCTION"] = "Olá! Você tem um cupom de desconto?";
-// env_ws_json["USE_COUPON_MESSAGE_INSERT"] = "Insira aqui o seu cupom de desconto KFC.";
-// env_ws_json.PAYMENT.MULTIPLE_CARDS_ACCEPTED_PAYING_METHODS = 'credito, debito';
-// env_ws_json["COLLECT_NAME_AMOUNT_OF_CHARACTERS"] = "40";
-// let CUSTOM_STEP_INTEGRATING = {
-//  "USES_CUSTOM_ICON": "false",
-//  "TITLE": "Pedido realizado!",
-//  "SUBTITLE": "Aguarde que estamos imprimindo sua comanda.\nFavor dirigir-se ao balcão de atendimento para retirada de seu cupom fiscal."
-// }
-//env_ws_json["CUSTOM_STEP_INTEGRATING"] = CUSTOM_STEP_INTEGRATING;
-// Seta Layout de Impressão
-// sel_lay_imp.value = 20;
-// Converte o JSON alterado em String e atribui ao Textarea indentado
+// Altera e/ou inclui Parametros
+env_ws_json["USE_PRINTER_BY_INTERFACE"] = "true";
+// Identa JSON e Inclui no TextArea
 env_ws.textContent = JSON.stringify(env_ws_json, null, 4);
 // Salva as Alterações
 save[1].click();
