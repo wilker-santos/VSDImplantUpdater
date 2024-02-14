33b6d14d5a16f4fb5693c68db9e751624d016022
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2024-02-14 10:00:48 -0300

    Update UpdateServicesBeta-2023.sh

--- UpdateServicesBeta-2023.sh
+++ UpdateServicesBeta-2023.sh
@@ -56,13 +56,50 @@ log "Instalando VS OS Interface...."
 sudo dpkg -i vs-os-interface_$VsOsInterface'_amd64.deb'
 log "Instalando VS Food Launcher...."
 sudo dpkg -i vs-food-launcher_2.0.0_amd64.deb
-# log "Instalando Google Chrome...."
-# sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-# IF Chrome Beta
-# log "Ajustando script vs-food"
-# wget --inet4-only -c https://raw.githubusercontent.com/wilker-santos/VSDImplantUpdater/main/vs-food.sh
-# sudo mv vs-food.sh /opt/videosoft/vs-food-launcher/app/vs-food.sh
+# Obtém o nome do modelo da CPU usando o comando lscpu
+cpu_model=$(lscpu | grep "Nome do modelo" | cut -d ':' -f 2 | sed 's/^ *//')
+
+# Converte o nome do modelo para minúsculas para facilitar a comparação
+cpu_model_lower=$(echo "$cpu_model" | tr '[:upper:]' '[:lower:]')
+
+# Verifica o modelo da CPU e instala o navegador correspondente
+if [[ $cpu_model_lower == *i3-5* ]]; then
+   # log "Download Chromium...."
+   # wget --inet4-only -c http://packages.linuxmint.com/pool/upstream/c/chromium/chromium_120.0.6099.216%7elinuxmint1%2buna_amd64.deb
+    log "Instalando Chromium...."
+    echo "Instalando Chromium...."
+    sudo snap install chromium
+    echo "Chromium Instalado...."
+   # sudo dpkg -i chromium_120.0.6099.216~linuxmint1+una_amd64.deb
+    log "Download Script VS Food For Chromium...."
+    wget -c "https://raw.githubusercontent.com/wilker-santos/VSDImplantUpdater/main/vs-food.sh"
+    log "Movendo Script VS Food For Chromium...."
+    sudo mv vs-food.sh /opt/videosoft/vs-food-launcher/app/vs-food.sh
+    chromium &>/dev/null &
+    sleep 5
+    killall chrome
+    sleep 3
+    # Nome do arquivo de configuração
+    arquivo_configuracao="/home/videosoft/snap/chromium/common/chromium/Default/Preferences"
+    # Adiciona o trecho no arquivo
+    sed -i -E 's/"autofill":\s*\{[^\}]*"last_version_deduped":\s*120\}/"autofill":\{"last_version_deduped":120,"profile_enabled":false\}/' "$arquivo_configuracao"
+    log "Desabilitado Preenchimento Automatico...."
+#if [[ $cpu_model_lower == *i5* ]]; then
+else
+    log "Download Google Chrome...."
+    wget --inet4-only -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    log "Instalando Google Chrome...."
+    sudo dpkg -i google-chrome-stable_current_amd64.deb
+    google-chrome --password-store=basic &>/dev/null &
+    sleep 5
+    killall chrome
+    # Nome do arquivo de configuração
+    arquivo_configuracao="/home/videosoft/.config/google-chrome/Default/Preferences"
+    # Adiciona o trecho no arquivo
+    sed -i -E 's/"autofill":\s*\{[^\}]*"last_version_deduped":\s*120\}/"autofill":\{"last_version_deduped":120,"profile_enabled":false\}/' "$arquivo_configuracao"
+    log "Desabilitado Preenchimento Automatico...."
+fi
 
 log "Removendo arquivos temporários...."
 # Remove packages
