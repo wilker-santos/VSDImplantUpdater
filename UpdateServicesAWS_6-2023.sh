fb741e5baa51ab9cfba45d5a1630fafba0b5e40a
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2024-02-23 13:16:45 -0300

    Update UpdateServicesAWS_6-2023.sh

--- UpdateServicesAWS_6-2023.sh
+++ UpdateServicesAWS_6-2023.sh
@@ -18,6 +18,7 @@ sudo rm /var/lib/dpkg/lock
 sudo ufw disable
 sudo modprobe usbcore autosuspend=-1
 xfconf-query -c xfwm4 -p /general/use_compositing -s false
+sudo apt install intel-media-va-driver -y
 # sudo snap remove brave
 # sudo apt remove google-chrome-stable -y
 
@@ -63,7 +64,7 @@ cpu_model=$(lscpu | grep "Nome do modelo" | cut -d ':' -f 2 | sed 's/^ *//')
 cpu_model_lower=$(echo "$cpu_model" | tr '[:upper:]' '[:lower:]')
 
 # Verifica o modelo da CPU e instala o navegador correspondente
-if [[ $cpu_model_lower == *i5* ]]; then
+if [[ $cpu_model_lower == *i* ]]; then
     log "Download Google Chrome...."
     wget --inet4-only -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     log "Instalando Google Chrome...."
