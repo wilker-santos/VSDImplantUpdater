a8f594e407b7b7a5a03deb24e485100f5d212507
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2024-02-23 13:56:36 -0300

    Update UpdateServicesAWS_6-2023.sh

--- UpdateServicesAWS_6-2023.sh
+++ UpdateServicesAWS_6-2023.sh
@@ -58,36 +58,36 @@ log "Instalando VS Food Launcher...."
 sudo dpkg -i vs-food-launcher_2.0.0_amd64.deb
 
 # Obtém o nome do modelo da CPU usando o comando lscpu
-cpu_model=$(lscpu | grep "Nome do modelo" | cut -d ':' -f 2 | sed 's/^ *//')
+#cpu_model=$(lscpu | grep "Nome do modelo" | cut -d ':' -f 2 | sed 's/^ *//')
 
 # Converte o nome do modelo para minúsculas para facilitar a comparação
-cpu_model_lower=$(echo "$cpu_model" | tr '[:upper:]' '[:lower:]')
+#cpu_model_lower=$(echo "$cpu_model" | tr '[:upper:]' '[:lower:]')
 
 # Verifica o modelo da CPU e instala o navegador correspondente
-if [[ $cpu_model_lower == *i* ]]; then
-    log "Download Google Chrome...."
-    wget --inet4-only -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    log "Instalando Google Chrome...."
-    sudo dpkg -i google-chrome-stable_current_amd64.deb
-elif [[ $cpu_model_lower == *A3* ]]; then
+#if [[ $cpu_model_lower == *i* ]]; then
+log "Download Google Chrome...."
+wget --inet4-only -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+log "Instalando Google Chrome...."
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+# elif [[ $cpu_model_lower == *A3* ]]; then
    # log "Download Chromium...."
    # wget --inet4-only -c http://packages.linuxmint.com/pool/upstream/c/chromium/chromium_120.0.6099.216%7elinuxmint1%2buna_amd64.deb
-    sudo apt update
-    log "Instalando Chromium...."
-    echo "Instalando Chromium...."
-    sudo snap install chromium
-    echo "Chromium Instalado...."
+   # sudo apt update
+   # log "Instalando Chromium...."
+   # echo "Instalando Chromium...."
+   # sudo snap install chromium
+   # echo "Chromium Instalado...."
    # sudo dpkg -i chromium_120.0.6099.216~linuxmint1+una_amd64.deb
-    log "Download Script VS Food For Chromium...."
-    wget -c "https://raw.githubusercontent.com/wilker-santos/VSDImplantUpdater/main/vs-food.sh"
-    log "Movendo Script VS Food For Chromium...."
-    sudo mv vs-food.sh /opt/videosoft/vs-food-launcher/app/vs-food.sh
-else
-    log "Download Google Chrome...."
-    wget --inet4-only -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    log "Instalando Google Chrome...."
-    sudo dpkg -i google-chrome-stable_current_amd64.deb
-fi
+   # log "Download Script VS Food For Chromium...."
+   # wget -c "https://raw.githubusercontent.com/wilker-santos/VSDImplantUpdater/main/vs-food.sh"
+   # log "Movendo Script VS Food For Chromium...."
+   # sudo mv vs-food.sh /opt/videosoft/vs-food-launcher/app/vs-food.sh
+# else
+   # log "Download Google Chrome...."
+   # wget --inet4-only -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+   # log "Instalando Google Chrome...."
+   # sudo dpkg -i google-chrome-stable_current_amd64.deb
+#fi
 
 
 
