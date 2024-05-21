1c8156a3e2a0e416bbfb52923e72cf7c5f1c3707
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2024-05-21 15:40:12 -0300

    Update UpdateServicesMania_5-2024.sh

--- UpdateServicesMania_5-2024.sh
+++ UpdateServicesMania_5-2024.sh
@@ -61,15 +61,6 @@ log "Instalando VS OS Interface...."
 sudo dpkg -i vs-os-interface_$VsOsInterface'_amd64.deb'
 log "Instalando VS Food Launcher...."
 sudo dpkg -i vs-food-launcher_2.0.0_amd64.deb
-
-# Obtém o nome do modelo da CPU usando o comando lscpu
-#cpu_model=$(lscpu | grep "Nome do modelo" | cut -d ':' -f 2 | sed 's/^ *//')
-
-# Converte o nome do modelo para minúsculas para facilitar a comparação
-#cpu_model_lower=$(echo "$cpu_model" | tr '[:upper:]' '[:lower:]')
-
-# Verifica o modelo da CPU e instala o navegador correspondente
-#if [[ $cpu_model_lower == *i* ]]; then
 log "Download Google Chrome...."
 wget --inet4-only -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 log "Instalando Google Chrome...."
