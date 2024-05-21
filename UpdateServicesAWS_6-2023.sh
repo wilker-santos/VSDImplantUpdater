6d774426e3032bfeceaafd31651a85a04e9bf06e
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2024-05-21 15:42:32 -0300

    Update UpdateServicesAWS_6-2023.sh

--- UpdateServicesAWS_6-2023.sh
+++ UpdateServicesAWS_6-2023.sh
@@ -61,46 +61,21 @@ log "Instalando VS OS Interface...."
 sudo dpkg -i vs-os-interface_$VsOsInterface'_amd64.deb'
 log "Instalando VS Food Launcher...."
 sudo dpkg -i vs-food-launcher_2.0.0_amd64.deb
-
-# Obtém o nome do modelo da CPU usando o comando lscpu
-#cpu_model=$(lscpu | grep "Nome do modelo" | cut -d ':' -f 2 | sed 's/^ *//')
-
-# Converte o nome do modelo para minúsculas para facilitar a comparação
-#cpu_model_lower=$(echo "$cpu_model" | tr '[:upper:]' '[:lower:]')
-
-# Verifica o modelo da CPU e instala o navegador correspondente
-#if [[ $cpu_model_lower == *i* ]]; then
 log "Download Google Chrome...."
 wget --inet4-only -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 log "Instalando Google Chrome...."
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-# elif [[ $cpu_model_lower == *A3* ]]; then
-   # log "Download Chromium...."
-   # wget --inet4-only -c http://packages.linuxmint.com/pool/upstream/c/chromium/chromium_120.0.6099.216%7elinuxmint1%2buna_amd64.deb
-   # sudo apt update
-   # log "Instalando Chromium...."
-   # echo "Instalando Chromium...."
-   # sudo snap install chromium
-   # echo "Chromium Instalado...."
-   # sudo dpkg -i chromium_120.0.6099.216~linuxmint1+una_amd64.deb
-   # log "Download Script VS Food For Chromium...."
-   # wget -c "https://raw.githubusercontent.com/wilker-santos/VSDImplantUpdater/main/vs-food.sh"
-   # log "Movendo Script VS Food For Chromium...."
-   # sudo mv vs-food.sh /opt/videosoft/vs-food-launcher/app/vs-food.sh
-# else
-   # log "Download Google Chrome...."
-   # wget --inet4-only -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-   # log "Instalando Google Chrome...."
-   # sudo dpkg -i google-chrome-stable_current_amd64.deb
-#fi
-
-
-
-# IF Chrome Beta
-# log "Ajustando script vs-food"
-# wget --inet4-only -c https://raw.githubusercontent.com/wilker-santos/VSDImplantUpdater/main/vs-food.sh
-# sudo mv vs-food.sh /opt/videosoft/vs-food-launcher/app/vs-food.sh
-
+# Caminho para o script
+SCRIPT_PATH="/opt/videosoft/vs-food-launcher/app/vs-food.sh"
+# Verifica se o script está usando o chromium
+if grep -q "chromium \"$VS_URL_APP\" $PARAMS" "$SCRIPT_PATH"; then
+    echo "Alterando de chromium para google-chrome..."
+    # Substitui 'chromium' por 'google-chrome'
+    sudo sed -i 's/chromium "$VS_URL_APP" $PARAMS/google-chrome "$VS_URL_APP" $PARAMS/g' "$SCRIPT_PATH"
+    echo "Alteração concluída."
+else
+    echo "O script já está usando o google-chrome."
+fi
 log "Removendo arquivos temporários...."
 # Remove packages
 rm *.deb
