fbd66c362631a0c0ea806483d56f1d42f33b4837
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2024-06-12 09:33:34 -0300

    Update alter-chromium.sh

--- alter-chromium.sh
+++ alter-chromium.sh
@@ -8,7 +8,7 @@ if grep -q "chromium \"$VS_URL_APP\" $PARAMS" "$SCRIPT_PATH"; then
     echo "Alterando de chromium para google-chrome..."
     
     # Substitui 'chromium' por 'google-chrome'
-    sed -i 's/chromium "$VS_URL_APP" $PARAMS/google-chrome "$VS_URL_APP" $PARAMS/g' "$SCRIPT_PATH"
+    sudo sed -i 's/chromium "$VS_URL_APP" $PARAMS/google-chrome "$VS_URL_APP" $PARAMS/g' "$SCRIPT_PATH"
     
     echo "Alteração concluída."
 else
