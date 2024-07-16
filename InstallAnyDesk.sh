5815eeaf7933c210d2acdc6242645276b5952534
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2024-07-16 16:35:22 -0300

    Update InstallAnyDesk.sh

--- InstallAnyDesk.sh
+++ InstallAnyDesk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# wget -O- https://raw.githubusercontent.com/wilker-santos/VSDImplantUpdater/main/DownloadAnyDesk.sh | bash
+# wget --inet4-only -O- https://raw.githubusercontent.com/wilker-santos/VSDImplantUpdater/main/InstallAnyDesk.sh | bash
 
 # URL da página HTML
 url="https://download.anydesk.com/linux/"
@@ -10,4 +10,4 @@ package=$(wget -qO- "$url" | grep -oP '(?<=<a href="./)[^"]*\.deb' | head -n 1)
 # Exibir a versão encontrada
 echo "$package"
 
-wget -c https://download.anydesk.com/linux/$package && sudo dpkg -i $package 
+wget --inet4-only -c https://download.anydesk.com/linux/$package && sudo dpkg -i $package 
