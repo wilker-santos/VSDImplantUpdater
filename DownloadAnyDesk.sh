544141175ae485391d1f96ba90f51fc620ee962f
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2024-07-16 16:32:30 -0300

    Create DownloadAnyDesk.sh

---
+++ DownloadAnyDesk.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# URL da página HTML
+url="https://download.anydesk.com/linux/"
+
+# Obter o conteúdo da página HTML usando wget e filtrar o primeiro link .deb encontrado
+package=$(wget -qO- "$url" | grep -oP '(?<=<a href="./)[^"]*\.deb' | head -n 1)
+
+# Exibir a versão encontrada
+echo "$package"
+
+wget -c https://download.anydesk.com/linux/$package && sudo dpkg -i $package 
