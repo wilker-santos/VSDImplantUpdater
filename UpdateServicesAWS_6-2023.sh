9aab434caddb504e6fc31b192125bba188e483e4
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2024-08-06 09:49:01 -0300

    Update UpdateServicesAWS_6-2023.sh

--- UpdateServicesAWS_6-2023.sh
+++ UpdateServicesAWS_6-2023.sh
@@ -14,7 +14,7 @@ VsAutoPagSE="2.31.2"
 VsFoodLauncher="2.0.0"
 # Output
 killall chrome
-wget --inet4-only -c https://images.food.vsd.app/uploads/10075/advertising/2024/05/09/8645317ab77b45f7.gif
+wget --inet4-only -c https://images.food.vsd.app/uploads/10075/advertising/2024/07/30/f304c1ba70673b9a.webp
 google-chrome --password-store=basic --kiosk --disable-pinch 8645317ab77b45f7.gif &>/dev/null &
 
 # Prepare
