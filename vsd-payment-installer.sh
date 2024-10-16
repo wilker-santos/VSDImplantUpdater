696b78dcc322132b278e6d18c773ed5b7da79b6f
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2024-10-16 16:39:53 -0300

    Update vsd-payment-installer.sh

--- vsd-payment-installer.sh
+++ vsd-payment-installer.sh
@@ -2,8 +2,8 @@
 # sudo wget --inet4-only -O- https://raw.githubusercontent.com/wilker-santos/VSDImplantUpdater/main/vsd-payment-installer.sh | bash
 
 sudo apt purge vsd-payment
-sudo mkdir /opt/vsd-payment/.config-bkp/
-sudo mv /opt/vsd-payment/.config/database.db /opt/vsd-payment/.config-bkp/database-bkp.db
+mkdir /opt/videosoft/backup/
+sudo mv /opt/vsd-payment/.config/database.db /opt/videosoft/backup/database-bkp.db
 wget -c https://cdn.vsd.app/softwares/vsd-payment/prod/vsd-payment_1.1.0_amd64.deb
 sudo dpkg -i vsd-payment_1.1.0_amd64.deb
 rm *deb
