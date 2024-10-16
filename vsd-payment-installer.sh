946648b7bbc30b2c630b1fb9cbb2cf44fa81d761
Wilker Santos <114582714+wilker-santos@users.noreply.github.com>
2024-10-16 15:55:19 -0300

    Update vsd-payment-installer.sh

--- vsd-payment-installer.sh
+++ vsd-payment-installer.sh
@@ -3,7 +3,6 @@
 
 sudo apt purge vsd-payment
 sudo rm /opt/vsd-payment/.config/database.db
-cd /tmp/
 wget -c https://cdn.vsd.app/softwares/vsd-payment/prod/vsd-payment_1.1.0_amd64.deb
 sudo dpkg -i vsd-payment_1.1.0_amd64.deb
 reboot
